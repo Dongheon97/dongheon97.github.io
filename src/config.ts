e9685a23c45402d4ecaab90879f501c6bc0e6c72
Dongheon97 <dongheon.lee97@gmail.com>
2026-01-01 19:59:17 -0500

    changed title

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
   name: "Dongheon Lee",
-  title: "Senior Software Engineer",
+  title: "Robotics Systems Researcher",
   description: "Portfolio website of Dongheon Lee",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "dongheon.lee97@gmail.com",
+    linkedin: "https://linkedin.com/in/dongheon97",
+    // twitter: "https://x.com/rfitzio",
+    github: "https://github.com/dongheon97",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
